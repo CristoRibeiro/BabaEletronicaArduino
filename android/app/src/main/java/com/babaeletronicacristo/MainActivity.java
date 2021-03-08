d0143d8204b9ec6a2908abc70c494fd6f0b0eb4e
CristoRibeiro <cristianosrsilva@gmail.com>
2021-03-07 22:57:22 -0300

    AddProjetoDefault

--- android/app/src/main/java/com/babaeletronicacristo/MainActivity.java
+++ android/app/src/main/java/com/babaeletronicacristo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.appgobarber;
+package com.babaeletronicacristo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "appGoBarber";
+    return "babaEletronicaCristo";
   }
 }
--- ios/babaEletronicaCristoTests/babaEletronicaCristoTests.m
+++ ios/babaEletronicaCristoTests/babaEletronicaCristoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface appGoBarberTests : XCTestCase
+@interface babaEletronicaCristoTests : XCTestCase
 
 @end
 
-@implementation appGoBarberTests
+@implementation babaEletronicaCristoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
